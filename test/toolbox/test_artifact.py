aea546530dbfc8f89d1f4ac8051eaafba942259f
Lilia Sfaxi <liliasfaxi@gmail.com>
2024-06-26 07:05:47 +0100

    delete print

--- test/toolbox/test_artifact.py
+++ test/toolbox/test_artifact.py
@@ -4,7 +4,6 @@ from culturalheritage.toolbox.artifact import Artifact
 class TestArtifact (unittest.TestCase):
     def setUp(self):
         self.artifact = Artifact()
-        print("Hello")
 
     def test_sum(self):
         self.assertEqual(self.artifact.sum(1,2),3)
